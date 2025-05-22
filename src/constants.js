3957a9de093334fcfcc50805c21e8be0a76347fa
Jakub Raus <jakub.raus@microsoft.wsei.edu.pl>
2025-05-22 17:36:09 +0200

    logic for dialogue texts, animations, camera scaling

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,7 @@
 export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `This is my PC. I work mostly in C# and JavaScript on which this game is built.
+    I would like to make more games in the future. I also play various games on it.
+    Here is my <a href="https://github.com/KubaRaus" target="_blank">GitHub</a> profile.`,
+};
